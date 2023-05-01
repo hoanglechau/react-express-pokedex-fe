cd740ec57addba272e59b4ba31f86b0a6d8d72b2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-01 18:19:00 +0700

    Update config.js

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,2 @@
-export const BASE_URL = import.meta.env
-  ? import.meta.env.VITE_REACT_APP_BACKEND_API
-  : "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_API;
 export const POKEMONS_PER_PAGE = 20;
